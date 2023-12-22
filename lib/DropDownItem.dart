25900996bbbb4da99f074c0b5b7ae3f006c952b2
Nate <Nate.Whittington@bottomline.com>
2023-12-21 18:18:04 -0600

    Initial commit - Flutter Task completed

---
+++ lib/DropDownItem.dart
@@ -0,0 +1,45 @@
+
+
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_locales/flutter_locales.dart';
+
+import 'main.dart';
+
+List<DropdownMenuItem<String>> get dropdownItems{
+  List<DropdownMenuItem<String>> menuItems = [
+
+
+  ];
+  return menuItems;
+}
+
+class DropdownItem extends StatefulWidget {
+
+
+  DropdownItem({super.key});
+
+
+
+
+  @override
+  State createState() => _DropdownItemState();
+
+}
+
+class _DropdownItemState extends State<DropdownItem> {
+  String selectedValue = "English";
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton(
+        value: selectedValue,
+        items: dropdownItems, onChanged: (String? newValue){
+      setState(() {
+        selectedValue = newValue!;
+      });
+    },
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_locales/flutter_locales.dart';
+import 'package:localization/DropDownItem.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Locales.init(['en', 'es']);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LocaleBuilder(
+        builder: (locale) => MaterialApp(
+              title: 'Localization Demo',
+              localizationsDelegates: Locales.delegates,
+              supportedLocales: Locales.supportedLocales,
+              locale: locale,
+              home: HomeScreen(),
+            ));
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<String> items = ['English', 'Spanish'];
+  String? selectedItems = 'English';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          DropdownButton<String>(
+              value: selectedItems,
+              items: items
+                  .map((item) =>
+                      DropdownMenuItem<String>(value: item, child: Text(item)))
+                  .toList(),
+              onChanged: (item) => setState(() {
+                    if (item == 'English') {
+                      Locales.change(context, 'en');
+                    } else {
+                      Locales.change(context, 'es');
+                    }
+                    selectedItems = item;
+                  }))
+        ],
+        title: const Text('Welcome'),
+        centerTitle: true,
+      ),
+      body: const Center(
+          child: LocaleText(
+        'hello',
+        style: TextStyle(fontSize: 20),
+      )),
+    );
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
